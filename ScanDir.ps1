20543206e235db094908998dafe070c5a6bd355f
C. David Littlejohn <cdlittlejohn1@gmail.com>
2021-08-11 12:00:43 -0400

    Added function help

--- ScanDir.ps1
+++ ScanDir.ps1
@@ -1,6 +1,46 @@
 #requires -Version 7
 
+<#
+.SYNOPSIS
+    Brute forces a list of web site directories and returns the ones that are successful
+
+.DESCRIPTION
+    Queries a provided list file for directories against a provided URL. Returns the
+    HTTP Status code of each result. The list file does not have to have extensions
+    such as .asp or .html as the script will enumerate them.
+
+.PARAMETER URL
+    The URL for the target. Provide the full http://www.* or https://www.* for best results.
+
+.PARAMETER PathList
+    The list of directories to tack onto the end of the URL.
+
+.PARAMETER ShowNonHits
+    A switch parameter that; by default, filters our directory queries that did not provide
+    infomation. Provide this parameter to show the attempts that did not return an HTTP Status
+    Code.
+
+.PARAMETER AllowAutoRedirect
+    A switch parameter that; by default, restricts the HTTP Request from redirecting. Provide
+    this paramter to show the redirects and their URLS.
+
+.EXAMPLE
+    ScanDir -URL 'http://www.google.com/' -List '.\dirpath.list'
+
+.EXAMPLE
+    ScanDir -URL 'http://www.google.com/' -List '.\dirpath.list' -AllowsAutoRedirect
+
+.EXAMPLE
+    ScanDir -URL 'http://www.google.com/' -List '.\dirpath.list' -ShowNonHits
+
+.OUTPUTS
+    PSCUSTOMOBJECT
+
+.NOTES
+    Author: ergo
+#>
 function ScanDir{
+    [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true,
         Position=0,
