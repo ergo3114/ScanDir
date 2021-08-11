5029ea92b4a097f66bfc43ff377e00a66197fe7a
C. David Littlejohn <cdlittlejohn1@gmail.com>
2021-08-11 09:39:37 -0400

    Initial Commit of ScanDir

---
+++ ScanDir.ps1
@@ -0,0 +1,85 @@
+#requires -Version 7
+
+function ScanDir{
+    param(
+        [Parameter(Mandatory=$true,
+        Position=0,
+        ValueFromPipeline=$true)]
+        [string]
+        $URL,
+
+        [Parameter(Mandatory=$true,
+        Position=1)]
+        [Alias("List")]
+        [ValidateScript({
+            if( -Not ($_ | Test-Path) ){
+                throw "File or folder does not exist"
+            }
+            return $true
+        })]
+        [System.IO.FileInfo]
+        $PathList,
+
+        [switch]
+        $ShowNonHits
+    )
+
+    BEGIN{
+        if($URL.EndsWith('/')){
+            $URL = $URL.Substring(0,$URL.Length-1)
+        }
+        $dirList = Get-Content $PathList
+        $formattedDirList = New-Object System.Collections.ArrayList
+        $dirList | ForEach-Object -ThrottleLimit 50 -Parallel {
+            $newList = $using:formattedDirList
+            $null = $newList.Add($_ + '.html')
+            $null = $newList.Add($_ + '.asp')
+            $null = $newList.Add($_ + '.aspx')
+            $null = $newList.Add($_ + '.txt')
+        }
+        $null = $formattedDirList.AddRange($dirList)
+        $results = New-Object System.Collections.ArrayList
+    }
+    PROCESS{
+        $formattedDirList | ForEach-Object -ThrottleLimit 15 -Parallel {
+            $results = $using:results
+            $dir = $_
+            if($dir.StartsWith('/')){
+                $dir = $dir.Substring(1)
+            }
+            if(![string]::IsNullOrWhitespace($dir)){
+                $URI = $using:URL + '/' + $dir
+                $httpRequest = [System.Net.WebRequest]::Create($URI)
+                $httpRequest.AllowAutoRedirect = $false
+                try{
+                    $httpResponse = $httpRequest.GetResponse()
+                    $httpStatus = [int]$httpResponse.StatusCode
+                }
+                catch{
+                    $httpStatus = -1
+                }
+
+                $obj = [pscustomobject]@{
+                    'URI' = $URI
+                    'dir' = $dir
+                    'HttpStatus' = $httpStatus
+                }
+                try{
+                    $null = $results.Add($obj)
+                } catch{
+                    Write-Verbose $Error[0]
+                }
+            }
+        }
+    }
+    END{
+        if($ShowNonHits){
+            $results | Sort-Object -Property HttpStatus -Descending
+        } else{
+            $results | Where-Object {$_.HttpStatus -ne -1} | Sort-Object -Property HttpStatus
+        }
+
+        if($null -eq $httpResponse){}
+        else{$httpResponse.Close()}
+    }
+}
